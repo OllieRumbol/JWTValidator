4937ed15efa8edc9ecae7f3728d19741e587e984
Oliver Bourne <otgb97@gmail.com>
2022-09-18 20:06:31 +0100

    react setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/index.css';
+
+ReactDOM.render(
+    <h1>Hello React!</h1>,
+    document.getElementById('app-root'),
+)
